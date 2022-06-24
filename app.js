803810fa6e71b5dc4ae405497b88d1cf33436605
Vinay Jadhav <vinay9168@gmail.com>
2022-06-24 14:46:28 +0530

    VinayJadhav

---
+++ app.js
@@ -0,0 +1,18 @@
+// let username=request.getParameter("username");
+// let password=request.getParameter("password");
+// System.out.println("username is "+username);
+// System.out.println("username is "+password);
+// console.log("Password"+password);
+// let a=prompt('Enter Password');
+// let b=prompt('Enter UserName');
+// let temp;
+// temp=a;
+// a=b;
+// b=temp;
+// console.log('The value of a after swapping: '+a);
+// console.log('The value of a after swapping '+b);
+console.log('Hello World');
+
+function msgprint() {
+    alert("You are Successfully Called the JavaScript function");
+}
